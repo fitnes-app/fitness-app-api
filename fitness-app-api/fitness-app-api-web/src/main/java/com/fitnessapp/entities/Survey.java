a52d264317e9448a03e3db05835a7325d478fba1
Erox <trugove@hotmail.com>
2018-11-29 00:42:15 +0100

    Survey changes and Tag done, problems with the get methods

--- fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/entities/Survey.java
+++ fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/entities/Survey.java
@@ -21,6 +21,8 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -29,92 +31,94 @@ import javax.xml.bind.annotation.XmlTransient;
  * @author Naluem
  */
 @Entity
-@Table(name="survey", catalog = "fitnessapp", schema = "public")
+@Table(name = "survey", catalog = "fitnessapp", schema = "public")
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 @NamedQueries({
-	@NamedQuery(name = "Survey.findAll", query = "SELECT s FROM Survey s"),
-	@NamedQuery(name = "Survey.findById", query = "SELECT s FROM Survey s WHERE s.id = :id"),
-	@NamedQuery(name = "Survey.findByDescription", query = "SELECT s FROM Survey s WHERE s.description = :description")})
+    @NamedQuery(name = "Survey.findAll", query = "SELECT s FROM Survey s"),
+    @NamedQuery(name = "Survey.findById", query = "SELECT s FROM Survey s WHERE s.id = :id"),
+    @NamedQuery(name = "Survey.findByDescription", query = "SELECT s FROM Survey s WHERE s.description = :description")})
 public class Survey implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	@Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Basic(optional = false)
-        @Column(nullable = false)
-	private Integer id;
-	@Size(max = 500)
-        @Column(length = 500)
-	private String description;
-	@OneToMany(mappedBy = "surveyId", fetch = FetchType.EAGER)
-	private List<Question> questionList;
-	@JoinColumn(name = "tag_id", referencedColumnName = "id")
-        @ManyToOne(fetch = FetchType.EAGER)
-	private Tag tagId;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Size(max = 500)
+    @Column(length = 500)
+    private String description;
+    @OneToMany(mappedBy = "surveyId", fetch = FetchType.LAZY)
+    @XmlTransient
+    private transient List<Question> questionList;
+    @JoinColumn(name = "tag_id", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tag tagId;
 
-	public Survey() {
-	}
+    public Survey() {
+    }
 
-	public Survey(Integer id) {
-		this.id = id;
-	}
+    public Survey(Integer id) {
+        this.id = id;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	@XmlTransient
-	public List<Question> getQuestionList() {
-		return questionList;
-	}
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
 
-	public void setQuestionList(List<Question> questionList) {
-		this.questionList = questionList;
-	}
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
 
-	public Tag getTagId() {
-		return tagId;
-	}
+    public Tag getTagId() {
+        return tagId;
+    }
 
-	public void setTagId(Tag tagId) {
-		this.tagId = tagId;
-	}
+    public void setTagId(Tag tagId) {
+        this.tagId = tagId;
+    }
+    
 
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (id != null ? id.hashCode() : 0);
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		// TODO: Warning - this method won't work in the case the id fields are not set
-		if (!(object instanceof Survey)) {
-			return false;
-		}
-		Survey other = (Survey) object;
-		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Survey)) {
+            return false;
+        }
+        Survey other = (Survey) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "com.fitnessapp.entities.Survey[ id=" + id + " ]";
-	}
-	
+    @Override
+    public String toString() {
+        return "com.fitnessapp.entities.Survey[ id=" + id + " ]";
+    }
+
 }
