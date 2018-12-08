4dd6a5b7549f5751c2c874a77c9f8e7e9af915a7
Jordi Godia <jgm17@alumnes.udl.cat>
2018-12-08 20:50:18 +0100

    merged with rest-api branc

--- fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
+++ fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
@@ -45,11 +45,11 @@ public class ApplicationConfig extends Application {
 		resources.add(com.fitnessapp.service.MeasurementFacadeREST.class);
 		resources.add(com.fitnessapp.service.MuscularGroupFacadeREST.class);
 		resources.add(com.fitnessapp.service.QuestionFacadeREST.class);
+		resources.add(com.fitnessapp.service.SpecialityFacadeREST.class);
 		resources.add(com.fitnessapp.service.SpecificTipFacadeREST.class);
 		resources.add(com.fitnessapp.service.SurveyFacadeREST.class);
 		resources.add(com.fitnessapp.service.TagFacadeREST.class);
-		resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
-                resources.add(com.fitnessapp.service.SpecialityFacadeREST.class);
+                resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
 	}
 	
 }
