d6acf357b82494c9c1206ab40bc9579f8633ed4f
Jordi Godia <jgm17@alumnes.udl.cat>
2018-12-31 16:17:07 +0100

    Added daily_basic_workout Entity, Facade and EntryPoint

--- fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
+++ fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
@@ -41,15 +41,16 @@ public class ApplicationConfig extends Application {
 		resources.add(com.fitnessapp.service.ClientFacadeREST.class);
 		resources.add(com.fitnessapp.service.CustomRoutineFacadeREST.class);
 		resources.add(com.fitnessapp.service.DailyAdvancedWorkoutFacadeREST.class);
+		resources.add(com.fitnessapp.service.DailyBasicWorkoutFacadeREST.class);
 		resources.add(com.fitnessapp.service.DailyTipFacadeREST.class);
 		resources.add(com.fitnessapp.service.MeasurementFacadeREST.class);
 		resources.add(com.fitnessapp.service.MuscularGroupFacadeREST.class);
-		resources.add(com.fitnessapp.service.QuestionFacadeREST.class);
+                resources.add(com.fitnessapp.service.QuestionFacadeREST.class);
 		resources.add(com.fitnessapp.service.SpecialityFacadeREST.class);
 		resources.add(com.fitnessapp.service.SpecificTipFacadeREST.class);
 		resources.add(com.fitnessapp.service.SurveyFacadeREST.class);
 		resources.add(com.fitnessapp.service.TagFacadeREST.class);
-        resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
+		resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
 	}
 	
 }
