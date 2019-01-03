b98817c5e8cca5e60ee06735950315fc075e967e
Jordi Godia <jgm17@alumnes.udl.cat>
2019-01-03 23:17:20 +0100

    ApplicationConfig fixed

--- fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
+++ fitness-app-api/fitness-app-api-web/src/main/java/com/fitnessapp/service/ApplicationConfig.java
@@ -32,15 +32,12 @@ public class ApplicationConfig extends Application {
 		resources.add(com.fitnessapp.service.AdministratorFacadeREST.class);
 		resources.add(com.fitnessapp.service.AdvancedClientTrackingFacadeREST.class);
 		resources.add(com.fitnessapp.service.AdvancedExerciseFacadeREST.class);
-		resources.add(com.fitnessapp.service.AdvancedWorkoutFacadeREST.class);
 		resources.add(com.fitnessapp.service.AnswerFacadeREST.class);
 		resources.add(com.fitnessapp.service.BasicClientTrackingFacadeREST.class);
 		resources.add(com.fitnessapp.service.BasicExerciseFacadeREST.class);
-		resources.add(com.fitnessapp.service.BasicWorkoutFacadeREST.class);
 		resources.add(com.fitnessapp.service.BodyTypeFacadeREST.class);
 		resources.add(com.fitnessapp.service.ClientFacadeREST.class);
 		resources.add(com.fitnessapp.service.CustomRoutineFacadeREST.class);
-		resources.add(com.fitnessapp.service.DailyAdvancedWorkoutFacadeREST.class);
 		resources.add(com.fitnessapp.service.DailyTipFacadeREST.class);
 		resources.add(com.fitnessapp.service.MeasurementFacadeREST.class);
 		resources.add(com.fitnessapp.service.MuscularGroupFacadeREST.class);
@@ -49,7 +46,7 @@ public class ApplicationConfig extends Application {
 		resources.add(com.fitnessapp.service.SpecificTipFacadeREST.class);
 		resources.add(com.fitnessapp.service.SurveyFacadeREST.class);
 		resources.add(com.fitnessapp.service.TagFacadeREST.class);
-        resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
+		resources.add(com.fitnessapp.service.TrainerFacadeREST.class);
 	}
 	
 }
